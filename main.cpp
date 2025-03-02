89c4ad64f5a3f847e13c095562de95139b048251
Takeda-Forever <85308264+Takeda-Forever@users.noreply.github.com>
2025-03-02 16:51:02 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,24 @@
 
 int main(int argc, char** argv)
 {
-  int a = argc, b = 2;
-  if(a == b)
-    return 0;
-  else
+  std::string username{};
+  std::string password{};
+  if(argc < 3)
+  {
+    std::cerr << "Invalid params!" << 
+        "Pls use like this: " << argv[0] << " <username> <password>\n"; 
     return 1;
-  return 1;
+  }
+  if(username.empty())
+  {
+    return 1;
+  }
+  if(password.empty())
+  {
+    return 1;
+  }
+  std::cout << "Username: << username << std::endl;
+  std::cout << "Password: << password << std::endl;
+    
+  return 0;
 }
